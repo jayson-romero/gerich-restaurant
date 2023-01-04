fd56e90e3f06663dac18344174e96113b4c03a40
ItonRomero <jaysonromero1218@gmail.com>
2023-01-04 22:53:45 +0800

    added Footer Component and Container

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,12 @@
 import SubHeading from './SubHeading/SubHeading';
-import NewsLetter from './Footer/Newsletter';
+import Newsletter from './Footer/Newsletter';
 import FooterOverlay from './Footer/FooterOverlay';
 import MenuItem from './Menuitem/MenuItem';
 import Navbar from './Navbar/Navbar';
 
 export {
       SubHeading,
-      NewsLetter,
+      Newsletter,
       FooterOverlay,
       MenuItem,
       Navbar
